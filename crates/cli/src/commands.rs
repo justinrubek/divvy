3df752e7cbea038afec86ef231c752f2e1c82480
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2024-03-25 18:32:25 -0500

    feat(cli): basic argument parsing

--- crates/cli/src/commands.rs
+++ crates/cli/src/commands.rs
@@ -1,26 +1,12 @@
+use std::path::PathBuf;
+
 #[derive(clap::Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub(crate) struct Args {
-    #[clap(subcommand)]
-    pub command: Commands,
-}
-
-#[derive(clap::Subcommand, Debug)]
-pub(crate) enum Commands {
-    Hello(Hello),
-}
-
-#[derive(clap::Args, Debug)]
-pub(crate) struct Hello {
-    #[clap(subcommand)]
-    pub command: HelloCommands,
-}
-
-#[derive(clap::Subcommand, Debug)]
-pub(crate) enum HelloCommands {
-    World,
-    Name {
-        #[arg()]
-        name: String,
-    },
+    // TODO: support 'k', 'm', 'g', 't' suffixes
+    /// Create files *byte_count* bytes in length.
+    #[arg(short, long)]
+    byte_count: usize,
+    /// The file to split.
+    file: PathBuf,
 }
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,8 +1,6 @@
-use crate::{
-    commands::{Commands, HelloCommands},
-    error::Result,
-};
+use crate::error::Result;
 use clap::Parser;
+use tracing::debug;
 
 mod commands;
 mod error;
@@ -12,19 +10,7 @@ async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
 
     let args = commands::Args::parse();
-    match args.command {
-        Commands::Hello(hello) => {
-            let cmd = hello.command;
-            match cmd {
-                HelloCommands::World => {
-                    println!("Hello, world!");
-                }
-                HelloCommands::Name { name } => {
-                    println!("Hello, {}!", name);
-                }
-            }
-        }
-    }
+    debug!("{:?}", args);
 
     Ok(())
 }
